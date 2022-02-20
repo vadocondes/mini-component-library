902cddd2418c509d80a6f03342ccbfc18b7af89d
arturo.miguel.next <arturo.miguel.next@bbva.com>
2022-02-20 21:38:22 +0100

    Progress Bar small component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,30 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+  <Wrapper 
+    role="progressbar" 
+    aria-valuenow={value}
+    aria-valuemin="0"
+    aria-valuemax="100">
+    <VisuallyHidden>{value}</VisuallyHidden>
+    <Bar style= {{ '--width': value + "%" }} />
+  </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};  
+  border-radius: 4px;
+  overflow: hidden;
+`
+
+const Bar = styled.div`
+  width: var(--width);
+  height: 8px;
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+`
 
 export default ProgressBar;
